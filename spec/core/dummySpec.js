1a8167d2bb6a4542c6b2c1cf789435d8481b253f
AlexisHFr <129600965+AlexisHFr@users.noreply.github.com>
2023-12-21 16:28:41 +0100

    scripts moved to archi-codex-scripts

--- spec/core/dummySpec.js
+++
@@ -1,7 +0,0 @@
-
-describe("a simple suite", function() {
-    it("that passes", function() {
-        console.log("inside jasmine test ! ");
-        expect( 100 ).toBeGreaterThan(10);
-    });
-});
